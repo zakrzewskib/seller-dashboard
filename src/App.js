8f9d21060c21a99c58b5ad616cf5b9faf0685329
Bartosz Zakrzewski <barzak200@gmail.com>
2021-12-05 11:13:02 +0100

    Add inital themes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 import Button from '@mui/material/Button';
+import { createTheme } from '@mui/material/styles';
+import { deepPurple } from '@mui/material/colors';
+import { ThemeProvider } from '@emotion/react';
+import { useState } from 'react';
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: deepPurple[400],
+      },
+    },
+  });
+
+  const lightTheme = createTheme({
+    palette: {
+      primary: {
+        main: '#FEAC34',
+      },
+    },
+  });
+
+  const [theme, setTheme] = useState(darkTheme);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained">Contained</Button>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Button variant="contained">Change theme</Button>
+    </ThemeProvider>
   );
 }
 
