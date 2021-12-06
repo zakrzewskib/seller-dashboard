5bf8d39619f639c92d123c0c061f518aad7bb957
Bartosz Zakrzewski <barzak200@gmail.com>
2021-12-06 13:38:35 +0100

    Change SellerChart to function component

--- src/components/SellerChart.js
+++ src/components/SellerChart.js
@@ -12,159 +12,125 @@ import MenuItem from '@mui/material/MenuItem';
 import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { useState } from 'react';
 
-export default class SellerChart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataOption: 'This week',
-      options: {
-        chart: {
-          type: 'column',
-        },
+export default function SellerChart(props) {
+  const [dataOption, setDataOption] = useState('This week');
+  const options = {
+    chart: {
+      type: 'column',
+    },
 
-        title: {
-          text: 'Total number of items sold last week',
-        },
+    title: {
+      text: 'Total number of items sold last week',
+    },
 
-        xAxis: {
-          categories: [
-            'Monday',
-            'Tuesday',
-            'Wednesday',
-            'Thursday',
-            'Friday',
-            'Saturday',
-            'Sunday',
-          ],
-        },
+    xAxis: {
+      categories: [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday',
+      ],
+    },
 
-        yAxis: {
-          title: {
-            //text: 'Total number of items sold',
-            text: '',
-          },
-        },
-
-        plotOptions: {
-          column: {
-            stacking: 'normal',
-          },
-        },
-
-        series: [
-          {
-            name: 'Total number of items sold',
-            data: [211, 451, 545, 123, 123, 1235, 555],
-          },
-        ],
+    yAxis: {
+      title: {
+        //text: 'Total number of items sold',
+        text: '',
       },
-      darkTheme: {
-        colors: ['blue'],
+    },
+
+    plotOptions: {
+      column: {
+        stacking: 'normal',
       },
-      lightTheme: {
-        colors: ['red'],
+    },
+
+    series: [
+      {
+        name: 'Total number of items sold',
+        data: [211, 451, 545, 123, 123, 1235, 555],
       },
-    };
-
-    // Highcharts.theme = this.state.darkTheme;
-  }
-
-  /*
-  Problems with changing theme of the chart in Highcharts:
-  1. How to use one of given themes by Highcharts (ex. Dark Unica)
-
-  2. You can't dynamically change theme - you can only set options again
-  - there should be a way of detecting when user clicks button to change theme:
-  one way of doing it is to change key and it will make component mount again
-  - also when opening/refreshing page theme is not set - you have to click switch button to make it change
-  */
-
-  // componentWillUnmount() {
-  //   console.log('umounted');
-  // }
-
-  componentDidMount() {
-    // Highcharts.theme = this.state.darkTheme;
-    // if (this.props.theme.name === 'darkTheme') {
-    //   Highcharts.theme = this.state.darkTheme;
-    // } else {
-    //   Highcharts.theme = this.state.lightTheme;
-    // }
-    //Highcharts.setOptions(Highcharts.theme);
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      dataOption: event.target.value,
-    });
+    ],
+  };
+  const darkTheme = {
+    colors: ['blue'],
+  };
+  const lightTheme = {
+    colors: ['red'],
   };
 
-  render() {
-    return (
-      <div>
-        <div
-          style={{
-            marginBottom: '24px',
-            display: 'flex',
-            gap: '12px',
-            height: '60px',
-            alignItems: 'center',
+  const handleChange = (event) => {
+    setDataOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <div
+        style={{
+          marginBottom: '24px',
+          display: 'flex',
+          gap: '12px',
+          height: '60px',
+          alignItems: 'center',
+        }}
+      >
+        <Button variant="contained" color="primary">
+          Bar graph
+        </Button>
+
+        <Button
+          variant="contained"
+          color="notActive"
+          sx={{
+            bgcolor: props.theme.palette.cardBackground,
+            color: props.theme.palette.lightGrey,
           }}
         >
-          <Button variant="contained" color="primary">
-            Bar graph
-          </Button>
+          Line graph
+        </Button>
 
-          <Button
-            variant="contained"
-            color="notActive"
-            sx={{
-              bgcolor: this.props.theme.palette.cardBackground,
-              color: this.props.theme.palette.lightGrey,
-            }}
-          >
-            Line graph
-          </Button>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <Typography color={props.theme.palette.lightGrey}>
+            TOTAL PROFIT
+          </Typography>
+          <Switch defaultChecked />
+          <Typography>NUMBER OF ITEMS</Typography>
+        </Stack>
 
-          <Stack direction="row" spacing={1} alignItems="center">
-            <Typography color={this.props.theme.palette.lightGrey}>
-              TOTAL PROFIT
+        <FormControlLabel
+          value="start"
+          control={<Checkbox />}
+          label={
+            <Typography color={props.theme.palette.lightGrey}>
+              INCLUDE PREVIOUS DATA
             </Typography>
-            <Switch defaultChecked />
-            <Typography>NUMBER OF ITEMS</Typography>
-          </Stack>
+          }
+          labelPlacement="start"
+        />
 
-          <FormControlLabel
-            value="start"
-            control={<Checkbox />}
-            label={
-              <Typography color={this.props.theme.palette.lightGrey}>
-                INCLUDE PREVIOUS DATA
-              </Typography>
-            }
-            labelPlacement="start"
-          />
-
-          <FormControl color="primary" variant="outlined">
-            <Select
-              color="primary"
-              variant="outlined"
-              value={this.state.dataOption}
-              onChange={this.handleChange}
-              inputProps={{ 'aria-label': 'Without label' }}
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={'Today'}>Today</MenuItem>
-              <MenuItem value={'This week'}>This week</MenuItem>
-              <MenuItem value={'This year'}>This year</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
-        <HighchartsReact highcharts={Highcharts} options={this.state.options} />
+        <FormControl color="primary" variant="outlined">
+          <Select
+            color="primary"
+            variant="outlined"
+            value={dataOption}
+            onChange={handleChange}
+            inputProps={{ 'aria-label': 'Without label' }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={'Today'}>Today</MenuItem>
+            <MenuItem value={'This week'}>This week</MenuItem>
+            <MenuItem value={'This year'}>This year</MenuItem>
+          </Select>
+        </FormControl>
       </div>
-    );
-  }
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  );
 }
