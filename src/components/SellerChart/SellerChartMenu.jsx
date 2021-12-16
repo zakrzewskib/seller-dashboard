ad7731dfb47bf89a43c4ecac36fc2c3b06d2f047
Bartosz Zakrzewski <barzak200@gmail.com>
2021-12-16 12:04:16 +0100

    Style include previous data

--- src/components/SellerChart/SellerChartMenu.jsx
+++ src/components/SellerChart/SellerChartMenu.jsx
@@ -1,16 +1,22 @@
+import React from "react";
+import { useState } from "react";
+
 import Typography from "@mui/material/Typography";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import React from "react";
-
 import MySelect from "../../atom-components/MySelect.jsx";
-
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Grid from "@mui/material/Grid";
 
 export default function SellerChartMenu(props) {
+  const [previousDataChecked, setPreviousDataChecked] = useState(false);
+
   const isMobile = useMediaQuery(props.theme.breakpoints.down("sm"));
   const includePreviousDataWidth = 233;
+
+  const includePreviousData = () => {
+    setPreviousDataChecked((prevState) => !prevState);
+  };
 
   return (
     <Grid
@@ -52,12 +58,28 @@ export default function SellerChartMenu(props) {
       <Grid item>
         <FormControlLabel
           value="start"
-          control={<Checkbox />}
+          control={
+            <Checkbox
+              sx={{
+                color: props.theme.palette.notActiveCheckBox,
+                "&.Mui-checked": {
+                  color: props.theme.palette.font,
+                },
+              }}
+            />
+          }
           label={
-            <Typography color={props.theme.palette.notActiveCheckBox}>
+            <Typography
+              color={
+                previousDataChecked
+                  ? props.theme.palette.font
+                  : props.theme.palette.notActiveCheckBox
+              }
+            >
               INCLUDE PREVIOUS DATA
             </Typography>
           }
+          onChange={includePreviousData}
           labelPlacement="start"
         />
       </Grid>
